5ab236954cf2baf3a445345bbe084207a3175c87
danielcordeirofaria <danielcordeirofaria@yahoo.com.br>
2025-03-08 14:50:06 -0300

    adding all structures

--- server.js
+++ server.js
@@ -7,13 +7,28 @@
  *************************/
 const express = require("express")
 const env = require("dotenv").config()
+
+const expressLayouts = require("express-ejs-layouts")
+
 const app = express()
 const static = require("./routes/static")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index Route
+app.get("/", (req, res) => {
+  res.render("index", { title: "First Page" })
+})
 
 /* ***********************
  * Local Server Information
